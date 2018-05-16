a8f9f6cd80a93fad3dbbe64bc080379a650bbd8f
Tyson Andre <tysonandre775@hotmail.com>
2018-05-15 19:46:16 -0700

    Document how to generate relative links to CSS, pages, etc.
    undo pelican changes, comment

--- pelican.conf.py
+++ pelican.conf.py
@@ -3,6 +3,7 @@
 
 AUTHOR = u"Monnand"
 SITENAME = u"Uniqush Blog"
+# The SITEURL can be commented out if you are rendering a version to run on a local server.
 SITEURL = 'http://blog.uniqush.org'
 
 TIMEZONE = 'Europe/Paris'
@@ -12,12 +13,12 @@ DEFAULT_LANG='en'
 # Blogroll
 LINKS =  (
           ('uniqush.org', 'http://uniqush.org'),
-          ('uniqush on github', 'http://github.com/uniqush'),
+          ('uniqush on github', 'https://github.com/uniqush'),
          )
 
 # Social widget
 SOCIAL = (
-            ('twitter', 'http://twitter.com/uniqush'),
+            ('twitter', 'https://twitter.com/uniqush'),
          )
 
 THEME = 'tuxlite_tbs' #'bootstrap'
